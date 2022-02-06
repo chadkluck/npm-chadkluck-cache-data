441f810228d05c194f8801c6fdde2dadcade6d33
chad-dev <kl5@chadkluck.net>
2022-02-05 21:05:05 -0600

    added index exports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const cache = require("lib/dao-cache.js");
+const tools = require("lib/tools.js");
+const endpoint = require("lib/dao-endpoint.js");
+
+module.exports = {
+	cache,
+	tools,
+	endpoint
+};
