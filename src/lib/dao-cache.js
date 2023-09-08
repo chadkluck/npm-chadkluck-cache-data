019e2b581e8f391fdb573a6fb2e90d1d50823614
chad-dev <kl5@chadkluck.net>
2023-09-07 21:52:49 -0500

    chore: set to aws sdk v2 as v3 is not yet fully tested in code

--- src/lib/dao-cache.js
+++ src/lib/dao-cache.js
@@ -32,17 +32,17 @@ const tools = require("./tools.js");
 /* AWS Functions */
 
 /* aws-sdk v2 */
-// const AWS = require("aws-sdk"); // included by aws so don't need to add to package.json except for devDependencies
-// const dynamo = new AWS.DynamoDB.DocumentClient();
-// AWS.config.update({region: process.env.AWS_REGION});
-// const s3 = new AWS.S3();
+const AWS = require("aws-sdk"); // included by aws so don't need to add to package.json except for devDependencies
+const dynamo = new AWS.DynamoDB.DocumentClient();
+AWS.config.update({region: process.env.AWS_REGION});
+const s3 = new AWS.S3();
 
 /* aws-sdk v3 */
-const { DynamoDB } = require("@aws-sdk/client-dynamodb");
-const { S3 } = require("@aws-sdk/client-s3");
+// const { DynamoDB } = require("@aws-sdk/client-dynamodb");
+// const { S3 } = require("@aws-sdk/client-s3");
 
-const dynamo = new DynamoDB({ region: process.env.AWS_REGION });
-const s3 = new S3();
+// const dynamo = new DynamoDB({ region: process.env.AWS_REGION });
+// const s3 = new S3();
 
 /* for hashing and encrypting */
 const crypto = require("crypto"); // included by aws so don't need to add to package.json
